2844b42b986bc825d03844c1a99805d898d86f97
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-06-19 17:02:13 +0200

    exploring how to create the hashed outputs

--- lib/atm.rb
+++ lib/atm.rb
@@ -2,15 +2,35 @@
 
 class Atm
   attr_accessor :balance
+  #attr_accessor :account_holder_id
+
+  success_output = Hash.new { |hash, key| hash[key] =  }
+  failure_output = {}
+  expected_output = { status: true, message: 'success', date: Date.today, amount: amount}
 
   def initialize
     @balance = 1000
-    @account_holder = 'bank'
+    @account_holder_id = 'Unknown account holder (unspecified account_holder_id).'
   end
 
-  def withdraw(amount, account)
-    @balance -= amount
-    @account_holder = account
+
+  #def withdraw(amount, account)
+  def withdraw(amount)
+    when amount <= balance
+      @balance -= amount
+
+      #@account_holder_id = account
+    else
+      expected_output = { status: false, message: 'Withdrawal failed, insufficient funds.', date: Date.today, amount: amount}
+    end
+    puts expected_output
   end
+
+
+  #def account(account_holder_id)
+  def account
+    @account_holder_id
+  end
+
 
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -7,25 +7,45 @@ describe Atm do
   let(:account) { class_double('Account') }
 
   before do
-    # "Before each test we need to add an attr of 'balance'
-    # to the 'account' object and set the value '100' "
+    # "Before each test we need to add an attribute of 'balance'
+    # to the 'account' object and set the value '1000' "
     #
     # why? Aaah.. it's just for testing purposes, it's like a reset.
-    allow(account).to receive(:balance).and_return(100)
+    allow(account).to receive(:balance).and_return(1000)
 
     # We also need to allow 'account' to receive the new balance
-    # using the setter method 'set_new_balance='
-    allow(account).to receive(:set_new_balance=)
+    # using the setter method 'set_new_balance!'
+    allow(account).to receive(:set_new_balance!)
+
+    # before assigning an account holder to the account we need
+    # to manage an unspecified account that belongs to the bank
+    #allow(account).to receive(:account_holder_id).and_return('unspecified account holder (unspecified account_holder_id)')
+    #allow(account).to receive(:set_new_account_holder_id!)
   end
 
 
+  # commenting out this 2nd init because it messes with our test init above
   #it 'has 1000$ on initialize' do
   #  expect(subject.balance).to eq 1000
   #end
 
 
-   it 'has an unspecified account that belongs to the bank'
-    expect(subject.account).to eq 'bank'
+   it 'has an unspecified account that belongs to the bank' do
+    # expect(subject.account).to eq 'bank'
+    #
+    # better clarify that is an account holder
+    # expect(subject.account_holder).to eq 'bank'
+    #
+    # why should 'account' or rather 'account_holder' be a method?
+    # isnt it better to have that as an object or variable constant? changing to constant
+    # expect(subject(account_holder)).to eq 'unspecified bank account'
+    #
+    # thinking about it we should create an object for this instead.
+    # lets initiate that object up top and set expectations here
+    expect(subject.account).to eq 'Unknown account holder (unspecified account_holder_id).'
+    # hey, thinking abt it this is not the account holder, it's just the account container,
+    # that will be connected to the account_holder object and it can continue as its own object
+
   end
 
 
@@ -36,7 +56,7 @@ describe Atm do
   end
 
 
-  it 'allow withdraw if account has enough balance' do
+  it 'allow withdrawal if account has enough balance' do
     # tell the test that how the expected hash-output from a successful withdrawal looks like
     expected_output = { status: true, message: 'success', date: Date.today, amount: 45}
 
