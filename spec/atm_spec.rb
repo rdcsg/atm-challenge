e09e4869d9be8b61e2079c5a263f327fac8f187f
Rodrigo Muñoz <rodrigo@rdcs.se>
2016-06-25 00:12:59 +0200

    stupid me. I had added the declaration of the expiration date in a test case and not the let statement at first, and then all the withdraw test failed cause I didnt include this fourth argument in the other tests. Gah.

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -19,40 +19,40 @@ describe Atm do
 
 
   it 'reduces funds at withdrawal' do
-    subject.withdraw(50, 1234, account, pre_set_exp_date: '04/17')
+    subject.withdraw(50, 1234, pre_set_exp_date, pre_set_exp_date, account)
     expect(subject.funds).to eq 950
   end
 
 
   it 'allows withdrawal if account has enough balance' do
     expected_output = { status: true, message: 'success', date: Date.today, amount: 45}
-    expect(subject.withdraw(45, 1234, account)).to eq expected_output
+    expect(subject.withdraw(45, 1234, pre_set_exp_date, account)).to eq expected_output
   end
 
 
   it 'reject withdrawal if account doesn\'t have enough balance' do
     expected_output = { status: false, message: 'insufficient funds', date: Date.today }
-    expect(subject.withdraw(105, 1234, account)).to eq expected_output
+    expect(subject.withdraw(105, 1234, pre_set_exp_date, account)).to eq expected_output
   end
 
 
   it 'rejects withdrawal if ATM has insufficient funds' do
     subject.funds = 50
     expected_output = { status: false, message: 'insufficient funds in ATM', date: Date.today }
-    expect(subject.withdraw(100, 1234, account)).to eq expected_output
+    expect(subject.withdraw(100, 1234, pre_set_exp_date, account)).to eq expected_output
   end
 
 
   it 'reject withdraw if pin is wrong' do
     expected_output = { status: false, message: 'wrong pin', date: Date.today }
-    expect(subject.withdraw(50, 9999, account)).to eq expected_output
+    expect(subject.withdraw(50, 9999, pre_set_exp_date, account)).to eq expected_output
   end
 
 
   it 'rejects withdrawal if card has expired' do
     allow(account).to receive(:pre_set_exp_date).and_return('12/15')
     expected_output = { status: false, message: 'denied, card expired.', date: Date.today }
-    expect(subject.withdraw(6, 1234, account)).to eq expected_output
+    expect(subject.withdraw(6, 1234, pre_set_exp_date, account)).to eq expected_output
   end
 
 
